f3786753ff7cc4b86fed608d3a0c8142b240986a
oonqt <r0blox1053@outlook.com>
2019-09-17 18:37:59 -0700

    Create log dir

--- logger.js
+++ logger.js
@@ -2,17 +2,21 @@ const fs = require("fs");
 const path = require("path");
 
 class Logger {
-    constructor(logType, logPath = null) {
+    constructor(logType, logPath) {
         this.logType = logType;
         this.logPath = logPath;
         this.timestamp = new Date();
-        this.logFile = path.join(logPath, `EmbyCord-${this.timestamp.getTime() / 1000 | 0}.txt`);
+        this.logFile = path.join(logPath, "logs", `EmbyCord-${this.timestamp.getTime() / 1000 | 0}.txt`);
     }
 
     log(message) {
         if(this.logType === "console") {
             console.log(message);
         } else if (this.logType === "file") {
+            if(!fs.existsSync(path.join(this.logPath, "logs"))) {
+                fs.mkdirSync(path.join(this.logPath, "logs"));
+            }
+
             if(!fs.existsSync(this.logFile)) {
                 fs.writeFileSync(this.logFile, `[${this.timestamp.toLocaleString()}]: ${message}\n`);
             } else {
