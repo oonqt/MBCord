fb6d8fc3939a7698c625f3dea6886ee79db244e6
oonqt <r0blox1053@outlook.com>
2019-12-31 12:39:03 -0800

    create device id

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,5 @@
-exports.toZero = (number) => {
-    return (`0${number}`).slice(-2);
-}
+const crypto = require("crypto");
+
+exports.toZero = number => (`0${number}`).slice(-2);
+
+exports.createDeviceId = version => crypto.createHash("md5").update(version).digest("hex");
