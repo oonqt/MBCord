2447d062591aa33c165a80019618038aeda3fe48
oonqt <r0blox1053@outlook.com>
2021-05-15 18:44:28 -1000

    recursive deep clean

--- utils/utils.js
+++ utils/utils.js
@@ -1,20 +1,4 @@
-/**
- *
- * @param {object} session MediaBrowser session object
- * @param {number} currentEpochSeconds Current epoch seconds (unix / 1000)
- *
- * @returns {number} Unix timestamp that the session ends at
- */
-exports.calcEndTimestamp = (session, currentEpochSeconds) =>
-	Math.round(
-		currentEpochSeconds +
-			Math.round(
-				(session.NowPlayingItem.RunTimeTicks -
-					session.PlayState.PositionTicks) /
-					10000 /
-					1000
-			)
-	);
+const clean = require('deep-cleaner');
 
 /**
  * 
@@ -26,7 +10,7 @@ exports.scrubObject = (object, ...keys) => {
 	
 	Object.assign(scrubbedObject, object);
 
-	keys.forEach((key) => delete scrubbedObject[key]);
+	clean(scrubbedObject, keys);
 
 	return scrubbedObject;
 }
